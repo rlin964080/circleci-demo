51bba51e0612ccf2c6f19bb6b24a8b5b7b32e8a2
Richard Lin <richard.lin@datasociety.com>
2021-04-21 17:13:18 -0400

    testing circleci for failures

--- test_words.py
+++ test_words.py
@@ -7,3 +7,6 @@ class TestWords:
     
     def test_concat_with_space(self):
         assert "Happy Birthday" == words.concat_with_space("Happy", "Birthday")
+    
+    def test_concat_fail(self):
+        assert "Will Fail" == words.concat_no_space("Will", "Fail")
